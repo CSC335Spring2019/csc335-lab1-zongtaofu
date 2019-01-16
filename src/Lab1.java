ea6dea87be763d5ecbe27b69b5cbcffd352d6904
lenovo <lenovo@10.134.218.160>
2019-01-16 08:17:06 -0700

    change the file by the comment said.

--- src/Lab1.java
+++ src/Lab1.java
@@ -8,7 +8,7 @@ public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm YOURNAME");
 	}
 
 }
